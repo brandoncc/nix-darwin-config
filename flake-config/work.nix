d65ad41782625b91e2454ef740513e0ef2fd46fd
Brandon Conway <brandoncc@gmail.com>
2025-02-11 20:51:39 -0800

    Setup work machine persistent apps

--- flake-config/work.nix
+++ flake-config/work.nix
@@ -5,6 +5,16 @@
 
   homebrew.onActivation.cleanup = "none"; # do not remove homebrew packages installed outside of nix
 
+  system.defaults.dock.persistent-apps = [
+    "/Applications/Logseq.app"
+    "/Applications/Google Chrome.app"
+    "/Applications/Shopify Mail.app"
+    "~/Applications/Chrome Apps/Taskarific.app"
+    "~/Applications/Chrome Apps/Workplace.app"
+    "/Applications/Shopify Calendar.app"
+    "/Applications/Google Drive.app"
+  ];
+
   system.defaults.dock.persistent-others = lib.mkAfter [
     "~/Screenshots"
   ];
