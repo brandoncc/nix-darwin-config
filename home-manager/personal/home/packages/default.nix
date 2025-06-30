5f6575ef6738f7b7d6d424947de7890e135c9599
Brandon Conway <brandoncc@gmail.com>
2025-06-29 21:59:00 -0700

    Add exercism to personal machine

--- home-manager/personal/home/packages/default.nix
+++ home-manager/personal/home/packages/default.nix
@@ -1,13 +1,14 @@
 {pkgs, ...}:
 
 {
-  home.packages = 
+  home.packages =
     with pkgs; [
       arduino-cli
       clang-tools
       devenv
       direnv
       discord
+      exercism
       heroku
       ngrok
       tree
