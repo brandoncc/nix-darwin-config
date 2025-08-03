e19d2ee727f78ef7724e1927d0acd91dcd46663d
Brandon Conway <brandoncc@gmail.com>
2025-08-03 14:25:12 -0700

    Symlink personal claude config from dotfiles, improve nix config

--- home-manager/personal/xdg/default.nix
+++ home-manager/personal/xdg/default.nix
@@ -3,17 +3,37 @@
 let
   dotfiles = "${config.home.homeDirectory}/.dotfiles";
 in  {
-  xdg.configFile."butler.nvim" = {
+  xdg.configFile.butler-nvim = {
     source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/butler.nvim";
     recursive = true;
   };
 
-  xdg.configFile."personal-zsh" = {
+  xdg.configFile.personal-claude-commands = {
+    source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/claude/personal/commands";
+    target = "../.claude/commands";
+  };
+
+  xdg.configFile.personal-claude-instructions = {
+    source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/claude/personal/instructions";
+    target = "../.claude/instructions";
+  };
+
+  xdg.configFile.personal-claude-md = {
+    source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/claude/personal/CLAUDE.md";
+    target = "../.claude/CLAUDE.md";
+  };
+
+  xdg.configFile.personal-claude-settings = {
+    source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/claude/personal/settings.json";
+    target = "../.claude/settings.json";
+  };
+
+  xdg.configFile.personal-zsh = {
     source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/zsh/.personal.zshrc";
     target = "../.zshrc"; # ~/.zshrc
   };
 
-  xdg.configFile."tmuxinator" = {
+  xdg.configFile.tmuxinator = {
     source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/tmuxinator/.tmuxinator";
     target = "../.tmuxinator"; # ~/.config/../.tmuxinator
     recursive = true;
