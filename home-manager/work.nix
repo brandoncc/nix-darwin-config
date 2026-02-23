91e22484d61d8236638bbf6853194d87f74073f4
Brandon Conway <brandon.conway@shopify.com>
2026-02-23 11:08:03 -0800

    Add linters for dotfiles to work machine

--- home-manager/work.nix
+++ home-manager/work.nix
@@ -1,7 +1,19 @@
-{ ... }:
+{ pkgs, lib, ... }:
 
 {
   imports = [ ./common ./work/git ./work/xdg ];
 
   home.sessionVariables.NIX_DARWIN_MACHINE_NAME = "work";
+
+  home.packages = with pkgs; lib.mkAfter [
+    # --- devenv replacements (remove once devenv is
+    # installed on work machine) ---
+    bats
+    luajitPackages.luacheck
+    shellcheck
+    shfmt
+    stylua
+    yamllint
+    # --- end devenv replacements ---
+  ];
 }
