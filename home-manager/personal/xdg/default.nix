420c5ada5b50af858e8e1937f3ead213e01b263a
Brandon Conway <brandoncc@gmail.com>
2026-03-29 10:05:05 -0700

    Symlink user-context.md for pi agent
    Add nix-darwin xdg entries for personal and work profiles to symlink
    the machine-specific user-context.md into ~/.pi/agent/.

--- home-manager/personal/xdg/default.nix
+++ home-manager/personal/xdg/default.nix
@@ -88,6 +88,11 @@ in  {
     target = "../.pi/agent/extensions";
   };
 
+  xdg.configFile.personal-pi-user-context = {
+    source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/pi/personal/user-context.md";
+    target = "../.pi/agent/user-context.md";
+  };
+
   xdg.configFile.personal-zsh = {
     source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/zsh/.personal.zshrc";
     target = "../.zshrc"; # ~/.zshrc
--- home-manager/work/xdg/default.nix
+++ home-manager/work/xdg/default.nix
@@ -78,6 +78,11 @@ in  {
     target = "../.pi/agent/extensions";
   };
 
+  xdg.configFile.work-pi-user-context = {
+    source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/pi/work/user-context.md";
+    target = "../.pi/agent/user-context.md";
+  };
+
   xdg.configFile.work-zsh = {
     source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/zsh/.work.zshrc";
     target = "../.zshrc";
